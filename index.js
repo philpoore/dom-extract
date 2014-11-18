310e0bfe4524a05d043f956dd01f9e3c45c64d0c
Phil <phil@byte22.com>
2014-11-18 17:23:18 +0000

    + Added support for using as libary + Extended the cli to include showing innerHTML

--- index.js
+++ index.js
@@ -1,52 +1,72 @@
-//
-// Dom Extract
-// philpoore - http://byte22.com - phil@byte22.com
-//
-// Extract HTML by css selector from FILE or STDIN
-//
-// Useage:
-//   dom-extract $selector [$file]
-//
-//	$selector 	- html css selector to extract
-//  $file		- file to read [optional]
-//
-//	If $file is ommited, then html is read from STDIN
-//
-// Examples:
-//
-//   dom-extract p file.html					: extract all 'p' tags from 'file.html'
-//   find *.html | xargs -L 1 dom-extract a		: extract all 'a' tags from '*.html' files
-//   cat file.html | dom-extract img			: extract 'img' tags from 'file.html'
-//
-
 var fs = require('fs');
-var colors = require('colors');
 var cheerio = require('cheerio');
 var readtoend = require('readtoend');
 
-var selector = process.argv[2];
-var filename = process.argv[3];
+var DomExtract = module.exports = function (opts){
+	
+	var text, selector, filename;
 
-if (!selector){
-	console.log("Selector Required".red);
-	process.exit(1);
-}
 
-function handle_data(err, data){
-	if (err){
-		console.log("Error reading file [%s]".red, filename);
-		throw err;
+	// initalize function
+	// can be used to re-init
+	var init = function (opts){
+		text = opts.text || false;
+		selector = opts.selector;
+		filename = opts.filename;
+
+		// we need a selector
+		if (!selector){
+			throw new Error('Selector required');
+		}
 	}
 
-	var $ = cheerio.load(data);
 
-	$(selector).each(function (i, el){
-		console.log($.html(el));
-	});
-}
+	// main function to extract results from data
+	// returns results array
+	var process_data = function(data){
+		var results = [];
+		var $ = cheerio.load(data);
 
-if (filename){
-	fs.readFile(filename, handle_data);
-}else{
-	readtoend.readToEnd(process.stdin, handle_data);
+		$(selector).each(function (i, el){
+			var line = text ? $(el).text() : $.html(el);
+			results.push(line);
+		});
+		return results;
+	}
+
+
+	// handles the input from filename or STDIN
+	// starts the process_data function
+	// logs the output to STDOUT
+	// used with cli / files
+	var input_handler = function(err, data){
+		if (err){
+			console.log("Error reading data".red);
+			throw err;
+		}
+
+		var results = process_data(data);
+		console.log(results.join('\n'));
+	}
+
+
+	// run the dom extract on given filename or STDIN
+	// used with cli / files
+	var run = function(){
+		if (filename){
+			fs.readFile(filename, input_handler);
+		}else{
+			readtoend.readToEnd(process.stdin, input_handler);
+		}	
+	}
+
+	// initialise with constructor opts
+	init(opts);
+
+	// return public methods
+	return {
+		init: init,				// allow re-init
+		process: process_data,	// for use from node.js / js
+		run: run				// for use from cli
+	}
 }
